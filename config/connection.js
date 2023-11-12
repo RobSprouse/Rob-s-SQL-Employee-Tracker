e955d777696e3d09a988dc29c3c32663c85a196f
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-11-11 20:39:51 -0500

    finally able to get null as a manager id to the employee

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
-import mysql from 'mysql2/promise.js';
-import { EventEmitter } from 'events';
+import mysql from "mysql2/promise.js";
+import { EventEmitter } from "events";
 
 const eventEmitter = new EventEmitter();
 let eTracker;
 
 const setupConnection = async () => {
-    eTracker = await mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "root",
-    });
-    eventEmitter.emit('connectionReady');
+     eTracker = await mysql.createConnection({
+          host: "localhost",
+          port: 3306,
+          user: "root",
+          password: "root",
+     });
+     eventEmitter.emit("connectionReady");
 };
 
 const setupDatabase = () => {
@@ -19,37 +19,34 @@ const setupDatabase = () => {
           try {
                const tables = [
                     `CREATE TABLE IF NOT EXISTS department (
-                id INT NOT NULL AUTO_INCREMENT, 
-                name VARCHAR(30) NOT NULL, 
-                PRIMARY KEY (id)
-              )`,
+                         id INT NOT NULL AUTO_INCREMENT,
+                         name VARCHAR(30) NOT NULL,
+                         PRIMARY KEY (id)
+                    )`,
                     `CREATE TABLE IF NOT EXISTS role (
-                id INT NOT NULL AUTO_INCREMENT,
-                title VARCHAR(30) NOT NULL,
-                salary DECIMAL NOT NULL, 
-                department_id INT NOT NULL, 
-                PRIMARY KEY (id), 
-                FOREIGN KEY (department_id) REFERENCES department(id)
-              )`,
+                         id INT NOT NULL AUTO_INCREMENT,
+                         title VARCHAR(30) NOT NULL,
+                         salary DECIMAL NOT NULL,
+                         department_id INT,
+                         PRIMARY KEY (id),
+                         FOREIGN KEY (department_id) REFERENCES department(id)
+                    )`,
                     `CREATE TABLE IF NOT EXISTS employee (
-                id INT NOT NULL AUTO_INCREMENT, 
-                first_name VARCHAR(30) NOT NULL, 
-                last_name VARCHAR(30) NOT NULL, 
-                role_id INT NOT NULL, 
-                manager_id INT DEFAULT NULL, 
-                PRIMARY KEY (id)
-              )`,
+                         id INT NOT NULL AUTO_INCREMENT,
+                         first_name VARCHAR(30) NOT NULL,
+                         last_name VARCHAR(30) NOT NULL,
+                         role_id INT,
+                         manager_id INT DEFAULT NULL,
+                         PRIMARY KEY (id),
+                         FOREIGN KEY (role_id) REFERENCES role(id),
+                         FOREIGN KEY (manager_id) REFERENCES employee(id)
+                    )`,
                ];
-
-               const alterTable = `ALTER TABLE employee 
-              ADD FOREIGN KEY (role_id) REFERENCES role(id),
-              ADD FOREIGN KEY (manager_id) REFERENCES employee(id)`;
 
                await eTracker.query("CREATE DATABASE IF NOT EXISTS employee_tracker");
                await eTracker.query("USE employee_tracker");
 
                await Promise.all(tables.map((table) => eTracker.query(table)));
-               await eTracker.query(alterTable);
 
                console.log("Employee Database Set-up and ready for use.");
           } catch (err) {
--- index.js
+++ index.js
@@ -6,10 +6,23 @@ import printTable from "console-table-printer";
 setupConnection();
 setupDatabase();
 
-async function eTrackerQuery (sqlQuery) {
+async function eTrackerPrint(sqlQuery) {
      const [rows, fields] = await eTracker.query(sqlQuery);
-     printTable.printTable(rows)
-};
+     if (rows.length === 0) {
+          console.log("\nThere are no records to display.\n");
+          return;
+     }
+     printTable.printTable(rows);
+}
+
+async function eTrackerGetValues(sqlQuery) {
+     const [rows, fields] = await eTracker.query(sqlQuery);
+     if (rows.length === 0) {
+          console.log("\nThere are no records to display.\n");
+          return;
+     }
+     return rows;
+}
 
 const options = [
      {
@@ -29,27 +42,81 @@ const options = [
      },
 ];
 
-const optionsHandler = (optionChoices) => {
+const optionsHandler = async (optionChoices) => {
      switch (optionChoices) {
           case "View All Departments":
-               return eTrackerQuery("SELECT * FROM department");
+               return eTrackerPrint("SELECT * FROM department");
           case "View All Roles":
-               return eTrackerQuery("SELECT * FROM role");
+               return eTrackerPrint("SELECT * FROM role");
           case "View All Employees":
-               return eTrackerQuery("SELECT * FROM employee");
+               return eTrackerPrint("SELECT * FROM employee");
+          case "Add a Department":
+               const addDepartmentAnswer = await inquirer.prompt(addDepartment);
+               await eTracker.query(`INSERT INTO department (name) VALUES ('${addDepartmentAnswer.departmentName}')`);
+               console.log(`\nThe department ${addDepartmentAnswer.departmentName} has been added to the database.\n`);
+               break;
+          case "Add a Role":
+               console.log("\nRefer to this Department Table when assigning the id of the Department to the role.");
+               await eTrackerPrint("SELECT * FROM department");
+               const addRoleAnswer = await inquirer.prompt(addRole);
+               await eTracker.query(
+                    `INSERT INTO role (title, salary, department_id) VALUES ('${addRoleAnswer.roleName}', '${addRoleAnswer.roleSalary}', '${addRoleAnswer.departmentId}')`
+               );
+               console.log(`\nThe role ${addRoleAnswer.roleName} has been added to the database.\n`);
+               break;
+          case "Add an Employee":
+               const roles = await eTrackerGetValues("SELECT * FROM role");
+               const roleChoices = roles.map((role) => ({ name: role.title, value: role.id }));
+               const managerChoices = await eTrackerGetValues("SELECT * FROM employee");
+               const managerNames = managerChoices.map((manager) => ({
+                    name: manager.first_name + " " + manager.last_name,
+                    value: manager.id,
+               }));
+
+               const addEmployeeAnswers = await inquirer.prompt([
+                    {
+                         type: "input",
+                         name: "addFirstName",
+                         message: "What is the first name of the employee?",
+                    },
+                    {
+                         type: "input",
+                         name: "addLastName",
+                         message: "What is the last name of the employee?",
+                    },
+                    {
+                         type: "list",
+                         name: "roleId",
+                         message: "Which role does this employee have?",
+                         choices: roleChoices,
+                    },
+                    {
+                         type: "list",
+                         name: "employeeManager",
+                         message: "Who is the employee's manager?",
+                         choices: [...managerNames, { name: "N/A", value: 0 }],
+                    },
+               ]);
+               await eTracker.execute(
+                    `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+                    [
+                         addEmployeeAnswers.addFirstName,
+                         addEmployeeAnswers.addLastName,
+                         addEmployeeAnswers.roleId,
+                         addEmployeeAnswers.employeeManager === 0 ? null : addEmployeeAnswers.employeeManager,
+                    ]
+               );
+
+               console.log(
+                    `\nThe employee ${addEmployeeAnswers.addFirstName} ${addEmployeeAnswers.addLastName} has been added to the database.\n`
+               );
+               break;
           case "Quit":
-               return ;
+               eTracker.end();
+               console.log("\nThank you for using the Employee Tracker. Goodbye!\n");
+               return process.exit(0);
      }
 };
-
-async function init() {
-     try {
-          const answers = await inquirer.prompt(options);
-          const sqlQuery = optionsHandler(answers.optionChoices);
-     } catch (err) {
-          console.error(err);
-     }
-}
 
 const addDepartment = [
      {
@@ -69,6 +136,11 @@ const addRole = [
           type: "input",
           name: "roleSalary",
           message: "What is the salary for this role?",
+     },
+     {
+          type: "input",
+          name: "departmentId",
+          message: "What is the department ID for this role?",
      },
 ];
 
@@ -94,5 +166,15 @@ const addEmployee = [
           message: "Who is the employee's manager?",
      },
 ];
+
+async function init() {
+     try {
+          const answers = await inquirer.prompt(options);
+          await optionsHandler(answers.optionChoices);
+          init();
+     } catch (err) {
+          console.error(err);
+     }
+}
 
 init();
